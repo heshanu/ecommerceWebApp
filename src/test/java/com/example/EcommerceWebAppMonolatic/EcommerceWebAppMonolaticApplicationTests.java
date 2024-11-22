d0e29b44d13a00de0fac8176868a00e49dcad6c1
heshanu <heshanu97@gmail.com>
2024-11-22 13:22:45 +0530

    first commit

---
+++ src/test/java/com/example/EcommerceWebAppMonolatic/EcommerceWebAppMonolaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceWebAppMonolatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebAppMonolaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
