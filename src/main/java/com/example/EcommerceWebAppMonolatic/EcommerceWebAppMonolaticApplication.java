a05fc0586790079cb0fce2b9539b84d28c0525d5
heshanu <heshanu97@gmail.com>
2024-11-22 13:22:45 +0530

    first commit

---
+++ src/main/java/com/example/EcommerceWebAppMonolatic/EcommerceWebAppMonolaticApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceWebAppMonolatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebAppMonolaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebAppMonolaticApplication.class, args);
+	}
+
+}
