78ebf5623dcbe08d3b5d24ba7c7a2297cbf475c9
heshanu <heshanu97@gmail.com>
2024-11-23 01:06:47 +0530

    implemented notification

--- src/main/java/com/example/EcommerceWebAppMonolatic/exception/handle/NotificationNotFoundExceptionHandler.java
+++ src/main/java/com/example/EcommerceWebAppMonolatic/exception/handle/NotificationNotFoundExceptionHandler.java
@@ -1,16 +1,17 @@
 package com.example.EcommerceWebAppMonolatic.exception.handle;
 
 import com.example.EcommerceWebAppMonolatic.dto.ErrorResponseDTO;
-import com.example.EcommerceWebAppMonolatic.exception.CustomerNotFoundException;
+import com.example.EcommerceWebAppMonolatic.exception.NotificationNotFoundException;
+import com.example.EcommerceWebAppMonolatic.exception.OrderNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class CustomerNotFoundExceptionHandler extends RuntimeException {
-    @ExceptionHandler(CustomerNotFoundException.class)
-    public ResponseEntity<ErrorResponseDTO> handleControllerNotFound(CustomerNotFoundException exception) {
+public class NotificationNotFoundExceptionHandler extends RuntimeException{
+    @ExceptionHandler(NotificationNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleControllerNotFound(NotificationNotFoundException exception) {
         ErrorResponseDTO errorResponse = ErrorResponseDTO.builder()
                 .errorMessage(exception.getMessage())
                 .errorCode(exception.getErrorCode())
--- src/main/java/com/example/EcommerceWebAppMonolatic/service/NotificationsService.java
+++ src/main/java/com/example/EcommerceWebAppMonolatic/service/NotificationsService.java
@@ -1,4 +1,12 @@
 package com.example.EcommerceWebAppMonolatic.service;
 
+import com.example.EcommerceWebAppMonolatic.dto.NotificationDTO;
+
+import java.util.List;
+
 public interface NotificationsService {
+    void createNotification(NotificationDTO notificationDTO);
+    List<NotificationDTO> allNotification();
+    NotificationDTO findNotificationById(Long notificationId);
+
 }
